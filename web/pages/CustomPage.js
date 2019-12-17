23234d4b29d9209e84aa942cbe64f1880573e7df
mcb-11561 <m.boyle.91x@gmail.com>
2019-12-17 18:50:44 -0500

    Route Change 2 (_1=FAIL)

--- web/pages/CustomPage.js
+++ web/pages/CustomPage.js
@@ -3,18 +3,19 @@ import PropTypes from 'prop-types';
 import Layout from '../components/Layout';
 import JotformEmbed from 'react-jotform-embed';
 
-class Services extends React.Component {
+class CustomPage extends React.Component {
     render () {
       const {config} = this.props
       return (
         <Layout config={config}> <div>
+      <JotformEmbed src="https://form.jotform.com/93215160810144"/>
       </div> </Layout>
       )
     }
   }
   
-  Services.propTypes = {
+  CustomPage.propTypes = {
     config: PropTypes.object
   }
   
-  export default Services
+  export default CustomPage
