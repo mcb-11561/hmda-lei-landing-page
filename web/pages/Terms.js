dbf5a6cb179816173ceb93d0bc5ccf6dafc94c31
mcb-11561 <m.boyle.91x@gmail.com>
2019-12-17 22:29:03 -0500

    Route Change 11 (Goodnight; Return)

--- web/pages/Terms.js
+++ web/pages/Terms.js
@@ -1,16 +1,13 @@
 import React from 'react';
-import { useRouter } from 'next/router';
 import PropTypes from 'prop-types';
 import Layout from '../components/Layout';
 
 class Terms extends React.Component {
     render () {
       const {config} = this.props
-      const router = useRouter()
-  const { Terms } = router.query
       return (
         <Layout config={config}> <div>
-    <p>Terms: {Terms}</p>
+    
       </div> </Layout>
       )
     }
@@ -19,6 +16,5 @@ class Terms extends React.Component {
   Terms.propTypes = {
     config: PropTypes.object
   }
-  
   
   export default Terms
