df768741e66d968df8a854c9806a3ba94cb6aa3a
mcb-11561 <m.boyle.91x@gmail.com>
2019-12-18 11:45:19 -0500

    Jotform Embed (Direct in Source Code)

--- web/pages/Services.js
+++ web/pages/Services.js
@@ -8,7 +8,7 @@ class Services extends React.Component {
       const {config} = this.props
       return (
         <Layout config={config}> <div>
-      
+      <JotformEmbed src="https://form.jotform.com/93215160810144"/>
       </div> </Layout>
       )
     }
