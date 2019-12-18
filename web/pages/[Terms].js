e8b157b5c3024cf64a8b56495865b76bef5bbaa5
mcb-11561 <m.boyle.91x@gmail.com>
2019-12-17 21:47:00 -0500

    Route Change 10 (Import Next/Router)

---
+++ web/pages/[Terms].js
@@ -0,0 +1,27 @@
+import React from 'react';
+import { useRouter } from 'next/router';
+import PropTypes from 'prop-types';
+import Layout from '../components/Layout';
+
+class Terms extends React.Component {
+    render () {
+      const {config} = this.props
+      const router = useRouter()
+  const { Terms } = router.query
+      return (
+        <Layout config={config}> <div>
+    <p>Terms: {Terms}</p>
+      </div> </Layout>
+      )
+    }
+  }
+  
+  Terms.propTypes = {
+    config: PropTypes.object
+  }
+  
+
+
+}
+  
+  export default Terms
