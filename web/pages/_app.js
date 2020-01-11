903145a44058c85d7e3cb1fa354200fa0e726a1d
mcb-11561 <m.boyle.91x@gmail.com>
2020-01-11 18:32:46 -0500

    favicon attempt 6 !!%

--- web/pages/_app.js
+++ web/pages/_app.js
@@ -47,71 +47,9 @@ class App extends BaseApp {
   render () {
     const {Component, pageProps} = this.props
     return (
-      <>
-      <Head>
-          {/* HTML Meta Tags */}
-          <meta
-            content="/images/favicon/android-chrome-256x256.png"
-            itemProp="image"
-            key="google-image"
-          />
-          {/* Facebook Meta Tags */}
-          <meta
-            content={`https://hmda-lei.com`}
-            key="facebook-url"
-            property="og:url"
-          />
-          <meta
-            content="/images/favicon/android-chrome-256x256.png"
-            key="facebook-image"
-            property="og:image"
-          />
-          {/* Twitter Meta Tags */}
-          <meta
-            content="summary_large_image"
-            key="twitter-card"
-            name="twitter:card"
-          />
-          <meta
-            content="/images/favicon/android-chrome-256x256.png"
-            key="twitter-iamge"
-            name="twitter:image"
-          />
-          {/* Favicon */}
-          <link
-            href="/images/favicon/apple-touch-icon.png"
-            rel="apple-touch-icon"
-            sizes="180x180"
-          />
-          <link
-            href="/images/favicon/favicon-32x32.png"
-            rel="icon"
-            sizes="32x32"
-            type="image/png"
-          />
-          <link
-            href="/images/favicon/favicon-16x16.png"
-            rel="icon"
-            sizes="16x16"
-            type="image/png"
-          />
-          <link href="/images/favicon/site.webmanifest" rel="manifest" />
-          <link
-            color="#5bbad5"
-            href="/images/favicon/safari-pinned-tab.svg"
-            rel="mask-icon"
-          />
-          <link href="/images/favicon/favicon.ico" rel="shortcut icon" />
-          <meta content="#00aba9" name="msapplication-TileColor" />
-          <meta
-            content="/images/favicon/browserconfig.xml"
-            name="msapplication-config"
-          />
-          <meta content="#ffffff" name="theme-color" /></Head>
       <Container>
         <Component {...pageProps} />
       </Container>
-      </>
     )
   }
 }
