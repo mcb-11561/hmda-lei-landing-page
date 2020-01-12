cb6296edfb4347bb0ce18f4b96d915b5ed2bc694
Taylor Bryant <taylor@bryant.io>
2020-01-12 12:55:53 -0600

    Move favicon markup to _app.js

--- web/pages/_app.js
+++ web/pages/_app.js
@@ -2,6 +2,7 @@ import React from 'react'
 import Router from 'next/router'
 import withGA from 'next-ga'
 import BaseApp from 'next/app'
+import Head from 'next/head'
 import client from '../client'
 // import 'normalize.css'
 import '../styles/shared.module.css'
@@ -45,7 +46,31 @@ class App extends BaseApp {
 
   render () {
     const {Component, pageProps} = this.props
-    return <Component {...pageProps} />
+    return (
+      <>
+        <Head>
+          {/* HTML Meta Tags */}
+          <meta content='/android-chrome-256x256.png' itemProp='image' key='google-image' />
+          {/* Facebook Meta Tags */}
+          <meta content={`https://hmda-lei.com`} key='facebook-url' property='og:url' />
+          <meta content='/android-chrome-256x256.png' key='facebook-image' property='og:image' />
+          {/* Twitter Meta Tags */}
+          <meta content='summary_large_image' key='twitter-card' name='twitter:card' />
+          <meta content='/android-chrome-256x256.png' key='twitter-iamge' name='twitter:image' />
+          {/* Favicon */}
+          <link href='/apple-touch-icon.png' rel='apple-touch-icon' sizes='180x180' />
+          <link href='/favicon-32x32.png' rel='icon' sizes='32x32' type='image/png' />
+          <link href='/favicon-16x16.png' rel='icon' sizes='16x16' type='image/png' />
+          <link href='/site.webmanifest' rel='manifest' />
+          <link color='#5bbad5' href='/safari-pinned-tab.svg' rel='mask-icon' />
+          <link href='/favicon.ico' rel='shortcut icon' />
+          <meta content='#00aba9' name='msapplication-TileColor' />
+          <meta content='/browserconfig.xml' name='msapplication-config' />
+          <meta content='#ffffff' name='theme-color' />
+        </Head>
+        <Component {...pageProps} />
+      </>
+    )
   }
 }
 
--- web/pages/_document.js
+++ web/pages/_document.js
@@ -13,67 +13,7 @@ export default class MyDocument extends Document {
   render () {
     return (
       <Html lang={this.props.lang || 'en'}>
-        <Head>
-      {/* HTML Meta Tags */}
-      <meta
-            content="/android-chrome-256x256.png"
-            itemProp="image"
-            key="google-image"
-          />
-          {/* Facebook Meta Tags */}
-          <meta
-            content={`https://hmda-lei.com`}
-            key="facebook-url"
-            property="og:url"
-          />
-          <meta
-            content="/android-chrome-256x256.png"
-            key="facebook-image"
-            property="og:image"
-          />
-          {/* Twitter Meta Tags */}
-          <meta
-            content="summary_large_image"
-            key="twitter-card"
-            name="twitter:card"
-          />
-          <meta
-            content="/android-chrome-256x256.png"
-            key="twitter-iamge"
-            name="twitter:image"
-          />
-          {/* Favicon */}
-          <link
-            href="/apple-touch-icon.png"
-            rel="apple-touch-icon"
-            sizes="180x180"
-          />
-          <link
-            href="/favicon-32x32.png"
-            rel="icon"
-            sizes="32x32"
-            type="image/png"
-          />
-          <link
-            href="/favicon-16x16.png"
-            rel="icon"
-            sizes="16x16"
-            type="image/png"
-          />
-          <link href="/site.webmanifest" rel="manifest" />
-          <link
-            color="#5bbad5"
-            href="/safari-pinned-tab.svg"
-            rel="mask-icon"
-          />
-          <link href="/favicon.ico" rel="shortcut icon" />
-          <meta content="#00aba9" name="msapplication-TileColor" />
-          <meta
-            content="/browserconfig.xml"
-            name="msapplication-config"
-          />
-          <meta content="#ffffff" name="theme-color" />
-          </Head>
+        <Head />
         <body>
           <Main />
           <NextScript />
