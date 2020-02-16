6d7daa15ab5c5c1b60d3aa52e6fa10c40bc95b99
mcb-11561 <m.boyle.91x@gmail.com>
2020-02-16 15:28:58 -0500

    Delete CLASS; Readd FUNCTION

--- web/pages/LookUp.js
+++ web/pages/LookUp.js
@@ -3,10 +3,8 @@ import PropTypes from 'prop-types'
 import Layout from '../components/Layout'
 import LookupForm from '../components/LookupForm'
 
-class LookUp extends React.Component {
-    render () {
-      const {config} = this.props
-      return (
+function LookUp({config}) {
+  return (
     <Layout config={config}>
       <div style={{width: '90%', maxWidth: '40rem', margin: '0 auto'}}>
         <LookupForm />
