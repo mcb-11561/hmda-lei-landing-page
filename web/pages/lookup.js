1d2f12e8086c29397baa5b445d681a4a66d39040
mcb-11561 <m.boyle.91x@gmail.com>
2020-02-16 15:07:55 -0500

    READD 'CLASS' LookUp to /LookUp

--- web/pages/lookup.js
+++ web/pages/lookup.js
@@ -3,8 +3,10 @@ import PropTypes from 'prop-types'
 import Layout from '../components/Layout'
 import LookupForm from '../components/LookupForm'
 
-function LookUp({config}) {
-  return (
+class LookUp extends React.Component {
+    render () {
+      const {config} = this.props
+      return (
     <Layout config={config}>
       <div style={{width: '90%', maxWidth: '40rem', margin: '0 auto'}}>
         <LookupForm />
